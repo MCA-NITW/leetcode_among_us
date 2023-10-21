63571309c839ec86f3bff647ec4fa65124cfcf3e
Sagar Gupta <sg85207@gmail.com>
2023-10-21 15:43:20 +0530

    Formatted

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
 
-const userNames = ["sagargupta1610" , "Awasthya" ,"lightmate" , "pranshu0801" ]
+const userNames = ["sagargupta1610", "Awasthya", "lightmate", "pranshu0801"];
 
 function App() {
   const url = "https://lcapi.cyclic.app/";
   const [data, setData] = useState([]);
 
-  const sortData = (data,mode) => {
+  const sortData = (data, mode) => {
     if (mode === "rating") {
-      return data.sort((a, b) => b.userContestRanking.rating - a.userContestRanking.rating);
+      return data.sort(
+        (a, b) => b.userContestRanking.rating - a.userContestRanking.rating,
+      );
+    } else if (mode === "rank") {
+      return data.sort(
+        (a, b) =>
+          a.userContestRanking.globalRanking -
+          b.userContestRanking.globalRanking,
+      );
     }
-    else if (mode === "rank") {
-      return data.sort((a, b) => a.userContestRanking.globalRanking - b.userContestRanking.globalRanking);
-    }
-
-  }
+  };
 
   const fetchInfo = () => {
     return Promise.all(
@@ -35,10 +39,9 @@ function App() {
     fetchInfo();
   }, []);
 
-
   console.log(data);
 
-  const ProfileCard = ({ data , rank }) => {
+  const ProfileCard = ({ data, rank }) => {
     const userName = data.userName;
     const contest = data.userContestRanking;
     return (
@@ -46,36 +49,48 @@ function App() {
         <div>{rank}</div>
         <div>{userName}</div>
         <div>{Math.round(contest.rating)}</div>
-        <div>{contest.globalRanking}/{contest.totalParticipants}</div>
+        <div>
+          {contest.globalRanking}/{contest.totalParticipants}
+        </div>
       </div>
     );
   };
 
-  return <div className="App">
-    <h1>LeetCode Leaderboard</h1>
-    <div className="leaderboard">
-      <div className="profile-sort">
-        <div>Sort By:</div>
-        <div>
-          <button onClick={() => setData(sortData(data,"rating"))}>Rating</button>
-          <button onClick={() => setData(sortData(data,"rank"))}>Rank</button>
+  return (
+    <div className="App">
+      <h1>LeetCode Leaderboard</h1>
+      <div className="leaderboard">
+        <div className="profile-sort">
+          <div>Sort By:</div>
+          <div>
+            <button onClick={() => setData(sortData(data, "rating"))}>
+              Rating
+            </button>
+            <button onClick={() => setData(sortData(data, "rank"))}>
+              Rank
+            </button>
+          </div>
         </div>
-      </div>
 
-      <div className="profile-row profile-header">
-        <div>Local Rank</div>
-        <div>Username</div>
-        <div>Contest Rating</div>
-        <div>Contest Ranking</div>
-      </div>
+        <div className="profile-row profile-header">
+          <div>Local Rank</div>
+          <div>Username</div>
+          <div>Contest Rating</div>
+          <div>Contest Ranking</div>
+        </div>
 
-      {data.map((profile) => {
-        return <ProfileCard data={profile} key={profile.userName} rank={data.indexOf(profile)+1} />;
-      })}
+        {data.map((profile) => {
+          return (
+            <ProfileCard
+              data={profile}
+              key={profile.userName}
+              rank={data.indexOf(profile) + 1}
+            />
+          );
+        })}
+      </div>
     </div>
-
-
-  </div>;
+  );
 }
 
 export default App;
