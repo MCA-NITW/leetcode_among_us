8741ff1bcdb83a7eaf3c8f7554f1edff47840c57
Sagar Gupta <92421383+Sagargupta16@users.noreply.github.com>
2023-12-19 11:00:28 +0530

    Added Global Ranking Querry Commented

--- client/src/api/GraphQLQueries.js
+++ client/src/api/GraphQLQueries.js
@@ -280,3 +280,52 @@ query getUserProfile($username: String!) {
         }
     }
 `;
+
+// query GetGlobalRankings($page: Int!) {
+//   globalRanking(page: $page) {
+//     totalUsers
+//     userPerPage
+//     myRank {
+//       ranking
+//       currentGlobalRanking
+//       currentRating
+//       dataRegion
+//       user {
+//         nameColor
+//         activeBadge {
+//           displayName
+//           icon
+//           __typename
+//         }
+//         __typename
+//       }
+//       __typename
+//     }
+//     rankingNodes {
+//       ranking
+//       currentRating
+//       currentGlobalRanking
+//       dataRegion
+//       user {
+//         username
+//         nameColor
+//         activeBadge {
+//           displayName
+//           icon
+//           __typename
+//         }
+//         profile {
+//           userAvatar
+//           countryCode
+//           countryName
+//           realName
+//           __typename
+//         }
+//         __typename
+//       }
+//       __typename
+//     }
+//     __typename
+//   }
+// }
+
